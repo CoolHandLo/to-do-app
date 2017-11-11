961cc2c783e580938dd069a370c135e311648e1c
CoolHandLo <lorengevans@gmail.com>
2017-11-10 19:35:53 -0700

    add delete button. error.

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 function onReady() {
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
   function createNewTodo() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) {return;}
-
+    id++;
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
 
     newToDoText.value = '';
@@ -18,19 +20,32 @@ function onReady() {
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
-    toDos.forEach(function(toDo) {
+    toDos.forEach(function(toDos) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteBtn = document.createElement('button');
       checkbox.type = 'checkbox';
-
-      newLi.textContent = toDo.title;
-
+      deleteBtn.innerHTML = '<span>Delete</span>';
+      newLi.textContent = toDos.title;
+      newLi.id = toDos.id;
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
+
+      deleteBtn.addEventListener('click', function() {
+        newLi.parentNode.removeChild(newLi);
+        const liId = this.id;
+        let deleteToDo = toDos.filter.(function(toDo) {
+          return todo.id === liId;
+        });
+        toDos.splice(deleteToDo, 1);
+        renderTheUI();
+      });
+
     });
+
 
   }
 
